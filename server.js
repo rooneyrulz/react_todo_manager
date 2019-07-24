bd8f00ecf49ceeb9f305e3d0b45f82402b403bc5
rooneyrulz <roony0782045336@gmail.com>
2019-07-25 01:57:52 +0530

    fixed some issues

--- server.js
+++ server.js
@@ -55,10 +55,10 @@ app.use('/api', authRoute);
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
